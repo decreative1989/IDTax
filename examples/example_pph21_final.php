7836d187b9415f21458e6cb6160f99692057b868
ade <ade@invosa.com>
2018-02-09 13:47:43 +0700

    Update Example file

--- examples/example_pph21_final.php
+++ examples/example_pph21_final.php
@@ -18,24 +18,26 @@ use QueenApp\Tax\PPh21Final;
 # Karyawan Mendapatkan Uang Pensiun Sebanyak 240 jt maka pajak atas penghasilan pensiunnya adalah
 
 # #Gross : Pajak ditanggung karyawan.
-$taxPension = new PPh21Final\PPh21Final(false);
-$taxPension->calculateTaxFinal('pesangon', 240000000);
-$tax = $taxPension->getFinalTax();
+$taxPesangon = new PPh21Final\PPh21Final(false);
+$taxPesangon->calculateTaxFinal('pesangon', 300000000);
+$tax = $taxPesangon->getFinalTax();
 
 echo '=== TAK METHODE GROSS ===';
-echo 'Base Tax :' . number_format($taxPension->getBaseTax(), 0, ',', '.');
+echo '<br/>';
+echo 'Base Tax :' . number_format($taxPesangon->getBaseTax(), 0, ',', '.');
 echo '<br/>';
 echo 'Pajak Yang Harus Dibayar :' . number_format($tax, 0, ',', '.');
 echo '<br/>';
 echo '<br/>';
 echo '<br/>';
 
-$taxPension = new PPh21Final\PPh21Final(true);
-$taxPension->calculateTaxFinal('pensiun', 240000000);
-$tax = $taxPension->getFinalTax();
-$taxAllowance = $taxPension->getFinalTaxAllowance();
+$taxPesangon = new PPh21Final\PPh21Final(true);
+$taxPesangon->calculateTaxFinal('pesangon', 300000000);
+$tax = $taxPesangon->getFinalTax();
+$taxAllowance = $taxPesangon->getFinalTaxAllowance();
 echo '=== TAK METHODE GROSS UP ===';
-echo 'Base Tax :' . number_format($taxPension->getBaseTax(), 0, ',', '.');
+echo '<br/>';
+echo 'Base Tax :' . number_format($taxPesangon->getBaseTax(), 0, ',', '.');
 echo '<br/>';
 echo 'Pajak Yang Harus Dibayar :' . number_format($tax, 0, ',', '.');
 echo '<br/>';
@@ -54,10 +56,11 @@ echo '<br/>';
 
 # #Gross : Pajak ditanggung karyawan.
 $taxPension = new PPh21Final\PPh21Final(false);
-$taxPension->calculateTaxFinal('pensiun', 240000000);
+$taxPension->calculateTaxFinal('pensiun', 300000000);
 $tax = $taxPension->getFinalTax();
 
 echo '=== TAK METHODE GROSS ===';
+echo '<br/>';
 echo 'Base Tax :' . number_format($taxPension->getBaseTax(), 0, ',', '.');
 echo '<br/>';
 echo 'Pajak Yang Harus Dibayar :' . number_format($tax, 0, ',', '.');
@@ -66,10 +69,11 @@ echo '<br/>';
 echo '<br/>';
 
 $taxPension = new PPh21Final\PPh21Final(true);
-$taxPension->calculateTaxFinal('pensiun', 240000000);
+$taxPension->calculateTaxFinal('pensiun', 300000000);
 $tax = $taxPension->getFinalTax();
 $taxAllowance = $taxPension->getFinalTaxAllowance();
 echo '=== TAK METHODE GROSS UP ===';
+echo '<br/>';
 echo 'Base Tax :' . number_format($taxPension->getBaseTax(), 0, ',', '.');
 echo '<br/>';
 echo 'Pajak Yang Harus Dibayar :' . number_format($tax, 0, ',', '.');
